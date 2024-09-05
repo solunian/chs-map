51626969248a8730ed15bc6c249f8242ed979f9c
Daniel Cui <solunian@gmail.com>
2024-09-04 19:28:30 -0700

    ops: add vercel analytics snippet

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,12 @@
   import "../app.css";
   import MusicPlayer from "$lib/components/MusicPlayer.svelte";
   import GithubLink from "$lib/components/GithubLink.svelte";
+
+  // vercel analytics snippet
+  import { dev } from "$app/environment";
+  import { inject } from "@vercel/analytics";
+
+  inject({ mode: dev ? "development" : "production" });
 </script>
 
 <div class="h-full w-full">
